30ad2b18aef7eb0ded05fc212e59b1dc7c20c933
William Kwok <wkwok16@uw.edu>
2019-01-12 18:33:33 -0800

    Updates

---
+++ precache-manifest.ab0af85caee9a18226c6afd2085d995a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e900e1ae7edcfff7588",
+    "url": "/WINFO-DataAud/static/js/runtime~main.1e900e1a.js"
+  },
+  {
+    "revision": "9de5d8e1f5ce4d703f12",
+    "url": "/WINFO-DataAud/static/js/main.9de5d8e1.chunk.js"
+  },
+  {
+    "revision": "abe63f36a6c3e3adc6ef",
+    "url": "/WINFO-DataAud/static/js/1.abe63f36.chunk.js"
+  },
+  {
+    "revision": "abe63f36a6c3e3adc6ef",
+    "url": "/WINFO-DataAud/static/css/1.36e5bfa5.chunk.css"
+  },
+  {
+    "revision": "de94ce5bfa7198a19628d15312ec29f9",
+    "url": "/WINFO-DataAud/index.html"
+  }
+];
